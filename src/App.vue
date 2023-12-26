cb6a11a11c7eda7daa17dd6d985aed364cd2c971
sausinfr <sebastien@sausin.fr>
2023-12-26 14:07:49 +0100

    layout du projet

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="relative h-screen flex text-white">
+    <div class="w-[13%] px-3 py-2 bg-black">Sidebar</div>
+    <div class="w-[87%] px-3 py-2 bg-neutral-900">Content</div>
+    <div class="absolute bottom-0 bg-neutral-900 w-full h-16 border-t border-neutral-800">Play bar</div>
+  </div>
+</template>
